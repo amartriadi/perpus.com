06e3ae7384cbcf5266b1eb54881341924e47fa9e
Gilang Hadi <gilangsaputro987@gmail.com>
2022-06-04 13:39:12 +0700

    fix sedikit

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,16 +2,19 @@
 
 @section('login')
     <div class="box shadow m-auto mt-5 p-4 rounded-3">
-        <div class="text-center">
-            <div class="itemsLogin d-flex justify-content-center">
+        <div class="">
+            <div class="itemsLogin d-flex text-center justify-content-center">
                 <h3 class="fs-1 fw-bold text-uppercase m-4 p-3">Perpus<span class="text-capitalize">dugam</span></h3>
             </div>
-            <form action="" class="text-center bg-transparent">
+            <form action="" class="bg-transparent">
                 <input type="email" class="formRegister d-block m-auto rounded-3 p-1 mb-4" id="email" placeholder="Email">
                 <input type="password" class="formRegister d-block m-auto rounded-3 p-1 mt-4" id="password"
                     placeholder="Password">
-                <input type="submit" id="submitRegister" class="registerSubmit m-auto mt-5 p-2">
-                <div class="createAkun mt-3 text-left">
+                <div class="mt-2 ms-5">
+                    <input type="checkbox" class="ms-2"> Show Password</input>
+                </div>
+                <input type="submit" id="submitRegister" class="registerSubmit m-auto mt-4 p-2">
+                <div class="createAkun mt-3 text-center">
                     <p>Belum Punya Akun? <a href="/register">Buat Akun</a></p>
                 </div>
             </form>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -3,16 +3,17 @@
 @section('register')
     <div class="boxRegis shadow m-auto mt-5 rounded-3 container">
         <div class="d-flex pt-2">
-            <div class=" d-block justify-content-center align-items-start col-6">
+            <div class=" d-block col-3 justify-content-center align-items-start col-6">
                 <h3 class="fs-1 fw-bold text-uppercase text-center pt-4">Perpus<span class="text-capitalize">dugam</span></h3>
                 <div class="d-flex justify-content-center">
                     <img src="./img/logo.png" alt="logo.png" width="180px" class="mt-2">
                 </div>
             </div>
-            <form action="" class="bg-transparent py-5">
+            <form action="" class="bg-transparent py-5 col-4">
                 <input type="email" class="formRegister d-block m-auto rounded-3 p-1 mb-4" id="email" placeholder="Email">
-                <input type="password" class="formRegister d-block m-auto rounded-3 p-1 mt-4" id="password"
+                <input type="password" class="formRegister d-block m-auto rounded-3 p-1 mt-4 mb-2" id="password"
                     placeholder="Password">
+                <input type="checkbox"> Show Password</input>
                 <input type="submit" id="submitRegister" class="registerSubmit m-auto mt-4 p-2">
                 <div class="createAkun mt-3 text-left">
                     <p>Sudah Punya Akun? <a href="/login">Login</a></p>
